2dd2addc82b87d6b3eac6727915024780600efd2
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-01-17 18:13:11 +0200

    Application finished

--- script.js
+++ script.js
@@ -8,18 +8,25 @@ let numberOfPeople = Number(numbersOfPeopleDiv.innerText)
 const calculateBill = () => {
   let bill = Number(billInput.value)
   let tipPercentage = Number(tipInput.value) / 100
-  let tipAmount = bill * tipPercentage 
+  let tipAmount = bill * tipPercentage
   let totalAmount = tipAmount + bill
   let totalPerPerson = totalAmount / numberOfPeople
-  totalPerPersonDiv.innerText = totalPerPerson
+  totalPerPersonDiv.innerText = `$${totalPerPerson.toFixed(2)}`
 }
 
 const increasePeople = () => {
-
+  numberOfPeople += 1
+  numbersOfPeopleDiv.innerText = numberOfPeople
+  calculateBill()
 }
 
 const decreasePeople = () => {
-
+  if(numberOfPeople <= 1){
+    throw('You should have atleast one person!')
+  }
+numberOfPeople -= 1
+numbersOfPeopleDiv.innerText = numberOfPeople
+calculateBill()
 
   if (poeple <= 1) {
     return
